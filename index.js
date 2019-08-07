15566a8ec4dd23e89ac0809c1b733f7877fd67f2
Fernanda Romero <fernandaromero85@gmail.com>
2019-08-07 20:24:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(currentLine, name){
 var placeInLine = currentLine.length + 1;
-var outputString = "Welcome, ” + name +”. You are number ” + placeInLine + ” in line.”;
+var outputString = "Welcome, " + name +". You are number " + placeInLine + " in line.";
 currentLine.push(name);
 return outputString;
 }
@@ -10,10 +10,10 @@ return outputString;
 function nowServing(currentLine){
 var outputString;
 if (currentLine.length === 0){
-outputString = “There is nobody waiting to be served!”
+outputString = “There is nobody waiting to be served!"
 }
 else{
-outputString = “Currently serving ” + currentLine[0] + “.”;
+outputString = “Currently serving " + currentLine[0] + “.";
 currentLine.shift();
 }
 return outputString;
@@ -22,17 +22,17 @@ return outputString;
 function currentLine(line){
 var outputString;
 if (line.length === 0){
-outputString = “The line is currently empty.”
+outputString = “The line is currently empty."
 }
 else{
-outputString = “The line is currently: ”
+outputString = “The line is currently: "
 for(var i = 0; i < line.length ; i++){
 if (i === 0){
 
-outputString = outputString + (i+1) + “. ” + line[i];
+outputString = outputString + (i+1) + “. " + line[i];
 }
 else{
-outputString = outputString + “, ” + (i+1) + “. ” + line[i];
+outputString = outputString + “, " + (i+1) + “. " + line[i];
 }
 }
 }
