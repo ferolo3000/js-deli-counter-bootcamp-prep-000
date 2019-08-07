cb6f9fecb8a05162c8a8c9978c2ef5f60cca37b7
Fernanda Romero <fernandaromero85@gmail.com>
2019-08-07 20:23:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(currentLine, name){
 var placeInLine = currentLine.length + 1;
-var outputString = “Welcome, ” + name +”. You are number ” + placeInLine + ” in line.”;
+var outputString = "Welcome, ” + name +”. You are number ” + placeInLine + ” in line.”;
 currentLine.push(name);
 return outputString;
 }
